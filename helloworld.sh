ca81706b250eefb4263123249806a55286fb6fe0
EmilioMuniz <emiliomunizphoto@gmail.com>
2021-01-27 03:23:56 +0000

    changes helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,16 @@
 echo hello world
+#!/bin/bash
+
+# Script Name:                  helloworld.sh 
+# Class Name:                   Ops 201
+# Author Name:                  Emilio Muniz
+# Date of latest revision:      1/26/2021
+# Purpose:                      Class 02 Challenge 
+
+# What is a variable?
+
+# Declaration
+my_variable=$(lspci)
+
+# Call
+echo $my_variable >>output.txt
