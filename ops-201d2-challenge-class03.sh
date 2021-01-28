3873a09bf6fdf12c10ac7cfa48913216b608d2cd
EmilioMuniz <emiliomunizphoto@gmail.com>
2021-01-28 00:32:01 +0000

    ops-201d2-challenge-class03.sh

---
+++ ops-201d2-challenge-class03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Login History                       
+# Emilio Muniz                      
+# 01/27/2021      
+# Print Login History      
+
+# Basic function
+print_loginhistory () {
+  last
+}
+print_loginhistory
