d4cda7e51995916f55fc934b4b25530d4c013ea9
EmilioMuniz <emiliomunizphoto@gmail.com>
2021-01-30 04:57:47 +0000

    ops-201d2-challenge-class05.sh

---
+++ ops-201d2-challenge-class05.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script:                   ops-201d2-challenge-class05                       
+# Author:                   Emilio Muniz                       
+# Date of latest revision:  1/29/2021    
+# Purpose:                  Use a loop.
+
+# Declaration of variables
+runpro="ps aux"
+
+
+
+
+
+# Declaration of array
+
+
+# Declaration of functions
+
+#loop
+
+for open in $runpro
+do
+     $runpro
+    
+done
+read -p "Enter PID"
+echo All done
+
+#Main
+
+# resources: https://linuxhandbook.com/bash-arrays/ 
+#End
